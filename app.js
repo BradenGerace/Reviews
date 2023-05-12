a8c891d20be65dd476d38d34fc955717f289cf50
Braden Gerace <braden.gerace@hotmail.com>
2023-05-12 14:10:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -65,3 +65,8 @@ prevBtn.addEventListener("click", function() {
     }
     showPerson(currentItem);
 });
+
+randomBtn.addEventListener("click", function() {
+    currentItem = (Math.floor((Math.random()) * reviews.length));
+    showPerson(currentItem);
+})
