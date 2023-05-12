4fdbc5b5ecb75ad0563b68f1b676c449db94dd0e
Braden Gerace <braden.gerace@hotmail.com>
2023-05-12 12:48:24 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const reviews = [
+
+];
+
+// select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
